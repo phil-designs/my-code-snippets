e432b0e22222bda86135edbafef94f292421e326
Phillip De Vita <phil@phildesigns.com>
2026-03-25 19:01:14 -0400

    Fix for masonry layout

--- wp-gallery-masonry-lightbox.php
+++ wp-gallery-masonry-lightbox.php
@@ -1,8 +1,8 @@
 <?php
-/*---------------------------------------------------
-	Masonry Gallery with Lightbox for WP Galleries
+/*---------------------------------
+	Masonry Gallery with Lightbox
 	Uses: Isotope, imagesLoaded, Fancybox
-------------------------------------------------------*/
+------------------------------------*/
 function aa_enqueue_gallery_scripts() {
 	global $post;
 	if ( is_a( $post, 'WP_Post' ) && has_shortcode( $post->post_content, 'gallery' ) ) {
@@ -25,9 +25,11 @@ function aa_masonry_gallery_shortcode( $output, $attr ) {
 		'include' => '',
 		'exclude' => '',
 		'columns' => 2,
+		'masonry' => '1',
 	), $attr, 'gallery' );
 
-	$id = intval( $atts['id'] );
+	$id      = intval( $atts['id'] );
+	$masonry = filter_var( $atts['masonry'], FILTER_VALIDATE_BOOLEAN );
 
 	if ( ! empty( $atts['include'] ) ) {
 		$include     = preg_replace( '/[^0-9,]+/', '', $atts['include'] );
@@ -60,15 +62,24 @@ function aa_masonry_gallery_shortcode( $output, $attr ) {
 	$gallery_id = 'aa-gallery-' . $gallery_counter;
 	$columns    = max( 1, min( 9, intval( $atts['columns'] ) ) );
 	$gutter     = 16;
-	$item_width = round( 100 / $columns, 4 ) . '% - ' . round( ( $columns - 1 ) * $gutter / $columns, 4 ) . 'px';
 
-	$html  = '<style>';
-	$html .= '#' . $gallery_id . ' { display: block; }';
-	$html .= '#' . $gallery_id . ' .aa-masonry-item { width: calc(' . $item_width . '); margin-bottom: ' . $gutter . 'px; box-sizing: border-box; }';
-	$html .= '#' . $gallery_id . ' .aa-masonry-item img { display: block; width: 100%; height: auto; }';
+	$html = '<style>';
+
+	if ( $masonry ) {
+		$item_width = round( 100 / $columns, 4 ) . '% - ' . round( ( $columns - 1 ) * $gutter / $columns, 4 ) . 'px';
+		$html .= '#' . $gallery_id . ' { display: block; }';
+		$html .= '#' . $gallery_id . ' .aa-gallery-item { width: calc(' . $item_width . '); margin-bottom: ' . $gutter . 'px; box-sizing: border-box; }';
+		$html .= '#' . $gallery_id . ' .aa-gallery-item img { display: block; width: 100%; height: auto; }';
+	} else {
+		$html .= '#' . $gallery_id . ' { display: grid; grid-template-columns: repeat(' . $columns . ', 1fr); gap: ' . $gutter . 'px; }';
+		$html .= '#' . $gallery_id . ' .aa-gallery-item { box-sizing: border-box; aspect-ratio: 1 / 1; overflow: hidden; }';
+		$html .= '#' . $gallery_id . ' .aa-gallery-item a { display: block; width: 100%; height: 100%; }';
+		$html .= '#' . $gallery_id . ' .aa-gallery-item img { display: block; width: 100%; height: 100%; object-fit: cover; }';
+	}
 	$html .= '</style>';
 
-	$html .= '<div id="' . esc_attr( $gallery_id ) . '" class="aa-masonry-gallery">';
+	$wrapper_class = $masonry ? 'aa-masonry-gallery' : 'aa-grid-gallery';
+	$html .= '<div id="' . esc_attr( $gallery_id ) . '" class="' . $wrapper_class . '">';
 
 	foreach ( $attachments as $attachment ) {
 		$full_src  = wp_get_attachment_image_src( $attachment->ID, 'full' );
@@ -76,34 +87,35 @@ function aa_masonry_gallery_shortcode( $output, $attr ) {
 		$alt       = get_post_meta( $attachment->ID, '_wp_attachment_image_alt', true );
 		$caption   = $attachment->post_excerpt;
 
-		$html .= '<div class="aa-masonry-item">';
+		$html .= '<div class="aa-gallery-item">';
 		$html .= '<a href="' . esc_url( $full_src[0] ) . '" data-fancybox="' . esc_attr( $gallery_id ) . '" data-caption="' . esc_attr( $caption ) . '">';
-		$html .= '<img src="' . esc_url( $thumb_src[0] ) . '" alt="' . esc_attr( $alt ) . '" width="' . esc_attr( $thumb_src[1] ) . '" height="' . esc_attr( $thumb_src[2] ) . '" />';
+		$html .= '<img src="' . esc_url( $thumb_src[0] ) . '" alt="' . esc_attr( $alt ) . '" width="' . esc_attr( $thumb_src[1] ) . '" height="' . esc_attr( $thumb_src[2] ) . '" class="wp-image-' . intval( $attachment->ID ) . '" />';
 		$html .= '</a>';
 		$html .= '</div>';
 	}
 
 	$html .= '</div>';
 
-	$html .= '<script>
-(function() {
+	if ( $masonry ) {
+		$init_js = '(function() {
 	var galleryEl = document.getElementById("' . esc_js( $gallery_id ) . '");
 	if ( ! galleryEl ) return;
-
 	imagesLoaded( galleryEl, function() {
 		new Isotope( galleryEl, {
-			itemSelector: ".aa-masonry-item",
+			itemSelector: ".aa-gallery-item",
 			layoutMode: "masonry",
-			masonry: {
-				columnWidth: ".aa-masonry-item",
-				gutter: 16
-			}
+			masonry: { columnWidth: ".aa-gallery-item", gutter: 16 }
 		});
 	});
-
 	Fancybox.bind("[data-fancybox=\"' . esc_js( $gallery_id ) . '\"]");
-})();
-</script>';
+})();';
+		wp_add_inline_script( 'fancybox', $init_js );
+	} else {
+		$init_js = '(function() {
+	Fancybox.bind("[data-fancybox=\"' . esc_js( $gallery_id ) . '\"]");
+})();';
+		wp_add_inline_script( 'fancybox', $init_js );
+	}
 
 	return $html;
 }
